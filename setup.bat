d652918ce5e05fad8a1af7a2f5815538643baa2d
caolei <cloutn@gmail.com>
2022-08-03 14:29:37 +0800

    缺少 7z.dll

--- setup.bat
+++ setup.bat
@@ -8,7 +8,7 @@
 
 cd "tool"
 
-"./7z.exe" x "../archive/python.7z" "-o../tool/python" -aoa -bso0 -bd
+"./7z/7z.exe" x "../archive/python.7z" "-o../tool/python" -aoa -bso0 -bd
 
 "./python/python" "script/setup.py"
 
