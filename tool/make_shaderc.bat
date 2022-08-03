100f3e2a5cc09d7e205a6e710d4623d59e8b0f78
caolei <cloutn@gmail.com>
2022-08-03 14:31:04 +0800

    改用 vs2022 编译

--- tool/make_shaderc.bat
+++ tool/make_shaderc.bat
@@ -6,7 +6,7 @@ mkdir build64
 cd build64
 
 rem "C:\Program Files\CMake\bin\cmake.exe" -G "Visual Studio 14 2015" -A Win32 -DSHADERC_ENABLE_SHARED_CRT:INT=1  -DSHADERC_SKIP_TESTS=ON -DSHADERC_SKIP_INSTALL=ON "../" 
-"%~dp0/cmake/bin/cmake.exe" -G "Visual Studio 14 2015" -DSHADERC_ENABLE_SHARED_CRT:INT=1  -DSHADERC_SKIP_TESTS=ON -DSHADERC_SKIP_INSTALL=ON "../" 
+"%~dp0/cmake/bin/cmake.exe" -G "Visual Studio 17 2022" -DSHADERC_ENABLE_SHARED_CRT:INT=1  -DSHADERC_SKIP_TESTS=ON -DSHADERC_SKIP_INSTALL=ON "../" 
 
 "%~dp0/cmake/bin/cmake.exe" --build . --config Debug
 
