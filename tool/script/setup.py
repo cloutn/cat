a295e58f62dc9021b763c80783499f57872fe364
caolei <cloutn@gmail.com>
2022-08-03 14:28:37 +0800

    缺少 7z.dll

--- tool/script/setup.py
+++ tool/script/setup.py
@@ -15,16 +15,16 @@ print(os.getcwd())
 #	"-bso0",  	# stadard output stream disabled
 #	"-bd"])		# disable progress indicator
 
-cmd(['./7z.exe', 'x', '../archive/cmake.7z', '-o../tool/cmake', '-aoa', '-bso0', '-bd'])
+cmd(['./7z/7z.exe', 'x', '../archive/cmake.7z', '-o../tool/cmake', '-aoa', '-bso0', '-bd'])
 
 #print("uncrompressing archive/free.7z")
-cmd(['./7z.exe', 'x', '../archive/free.7z', '-o../free/', '-aoa', '-bso0', '-bd'])
+cmd(['./7z/7z.exe', 'x', '../archive/free.7z', '-o../free/', '-aoa', '-bso0', '-bd'])
 
 #print("uncrompressing testCat/bin/bin.7z")
-cmd(['./7z.exe', 'x', '../archive/bin.7z', '-o../testCat/bin', '-aoa', '-bso0', '-bd'])
+cmd(['./7z/7z.exe', 'x', '../archive/bin.7z', '-o../testCat/bin', '-aoa', '-bso0', '-bd'])
 
 #print("uncrompressing testCat/bin64/bin64.7z")
-cmd(['./7z.exe', 'x', '../archive/bin64.7z', '-o../testCat/bin64', '-aoa', '-bso0', '-bd'])
+cmd(['./7z/7z.exe', 'x', '../archive/bin64.7z', '-o../testCat/bin64', '-aoa', '-bso0', '-bd'])
 
 #print("uncrompressing testCat/art/art.7z")
-cmd(['./7z.exe', 'x', '../archive/art.7z', '-o../testCat/art', '-aoa', '-bso0', '-bd'])
+cmd(['./7z/7z.exe', 'x', '../archive/art.7z', '-o../testCat/art', '-aoa', '-bso0', '-bd'])
